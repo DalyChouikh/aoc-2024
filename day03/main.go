ddd2a512e6e210f17eb31369d819d2840046babe
DalyChouikh <chouikhdaly215@gmail.com>
2024-12-06 19:07:19 +0100

    SOLVED: day03-part2

--- day03/main.go
+++ day03/main.go
@@ -9,9 +9,23 @@ import (
 	"github.com/DalyChouikh/aoc-2024/file"
 )
 
-var pattern = regexp.MustCompile(`mul\((?<op1>[0-9]+)\,(?<op2>[0-9]+)\)`)
+var mulPattern = regexp.MustCompile(`mul\((?<op1>[0-9]+)\,(?<op2>[0-9]+)\)`)
+var doPattern = regexp.MustCompile(`do\(\)`)
+var dontPattern = regexp.MustCompile(`don't\(\)`)
+var combinedPattern = regexp.MustCompile(`mul\([0-9]+,[0-9]+\)|don\'t\(\)|do\(\)`)
 
-func extractSubmatches(input string) [][]string {
+func convertToStringSlice(intSlice [][]int) [][]string {
+	stringSlice := make([][]string, len(intSlice))
+	for i, row := range intSlice {
+		stringSlice[i] = make([]string, len(row))
+		for j, val := range row {
+			stringSlice[i][j] = strconv.Itoa(val)
+		}
+	}
+	return stringSlice
+}
+
+func extractSubmatches(input string, pattern *regexp.Regexp) [][]string {
 	return pattern.FindAllStringSubmatch(input, -1)
 }
 
@@ -25,7 +39,7 @@ func parseOperands(match []string) (int, int, error) {
 }
 
 func processString(input string) (int, error) {
-	submatches := extractSubmatches(input)
+	submatches := extractSubmatches(input, mulPattern)
 	result := 0
 	for _, match := range submatches {
 		op1, op2, err := parseOperands(match)
@@ -34,6 +48,31 @@ func processString(input string) (int, error) {
 		}
 		result += op1 * op2
 	}
+	return result, nil
+}
+
+func processStringWithDoAndDont(input string) (int, error) {
+	result := 0
+	mulEnabled := true
+
+	matches := combinedPattern.FindAllString(input, -1)
+
+	for _, match := range matches {
+		switch {
+		case mulPattern.MatchString(match):
+			if mulEnabled {
+				submatch := mulPattern.FindStringSubmatch(match)
+				op1, _ := strconv.Atoi(submatch[1])
+				op2, _ := strconv.Atoi(submatch[2])
+				result += op1 * op2
+			}
+		case doPattern.MatchString(match):
+			mulEnabled = true
+		case dontPattern.MatchString(match):
+			mulEnabled = false
+		}
+	}
+
 	return result, nil
 }
 
@@ -49,14 +88,32 @@ func processInput(inputs []string) (int, error) {
 	return totalResult, nil
 }
 
+func processInputWithDoAndDont(inputs []string) (int, error) {
+	totalResult := 0
+	for _, input := range inputs {
+		result, err := processStringWithDoAndDont(input)
+		if err != nil {
+			return 0, err
+		}
+		totalResult += result
+	}
+	return totalResult, nil
+}
+
 func main() {
 	input, err := file.ReadInput("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	result, err := processInput(input)
+	result1, err := processInput(input)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(result)
+	result2, err := processInputWithDoAndDont(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result1)
+	fmt.Println(result2)
 }
